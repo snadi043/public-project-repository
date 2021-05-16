5fae4bd25b8484d27501d1647c920918458daa04
snadi043 <60456049+snadi043@users.noreply.github.com>
2021-05-16 11:30:47 -0400

    final changes done
    updated with final changes for the script files

--- script.js
+++ script.js
@@ -11,12 +11,17 @@
         .then((photos) =>{
            for(let i = 0; i < 10; i++){
                gallery.innerHTML += `
-               <div class="card col-3 m-2 mx-auto">
+               <div class="card col-3 m-2 mx-auto" 
+               style="background-color: #181818; 
+               color: #fff;
+               text-align: center;
+               border: 4px solid #fff;
+               padding: 15px 10px;">
                <br><br>
                
 
                 <a target="_self" href="${photos[num].thumbnailUrl}">
-                <img src="${photos[num].thumbnailUrl}" alt="photo" width="300">
+                <img src="${photos[num].thumbnailUrl}" alt="photo" style="width="300px"/>
                 </a>
 
                 <div class="card-body">
